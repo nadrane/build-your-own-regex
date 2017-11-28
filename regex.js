b29dbe753c44eccccd252bc2793f083d758eae30
Nicholas Drane <nicholasdrane@gmail.com>
2017-11-27 22:19:52 -0600

    support '?'

--- regex.js
+++ regex.js
@@ -1,30 +1,41 @@
 function matchOne(pattern, text) {
-  if (text.length === 0) return false;
+  if (!text) return false;
   return pattern[0] === "." || text[0] === pattern[0];
 }
 
 function search(pattern, text) {
   if (pattern[0] === "^") {
     return match(pattern.slice(1), text);
-  } else if (pattern.length > 0){
+  } else {
     // If the pattern works given any starting location in the text
     return text.split('').some((_, index) => {
       return match(pattern, text.slice(index))
     })
-  } else {
-    return match(pattern, text)
   }
 }
 
 function match(pattern, text) {
   if (!pattern) return true;
   else if (!text && pattern === "$") return true;
+  else if (pattern.length > 1 && '?' === pattern[1]) {
+    return matchQuestion(pattern, text)
+  }
   else {
     return matchOne(pattern[0], text[0]) && match(pattern.slice(1), text.slice(1));
   }
 }
 
+function matchQuestion(pattern, text) {
+  if (matchOne(pattern[0], text[0]) && match(pattern.slice(2), text.slice(1))) {
+    return true
+  } else {
+    return match(pattern.slice(2), text)
+  }
+}
+
 module.exports = {
   matchOne,
+  matchQuestion,
+  match,
   search
 };
--- test.js
+++ test.js
@@ -1,74 +1,100 @@
 const { expect } = require("chai");
 const mocha = require("mocha");
-const { matchOne, search } = require("./regex");
+const { matchOne, match, search, matchQuestion } = require("./regex");
 
-describe("regex", function() {
-  describe("matchOne", function() {
-    it("should return true when the text matches the pattern.", function() {
+describe("regex", () => {
+  describe("matchOne", () => {
+    it("should return true when the text matches the pattern.", () => {
       const pattern = "a";
       const text = "a";
       expect(matchOne(pattern, text)).to.equal(true);
     });
-    it("should return false when the text is an empty string", function() {
+    it("should return false when the text is an empty string", () => {
       const pattern = "b";
       const text = "";
       expect(matchOne(pattern, text)).to.equal(false);
     });
-    it("should return false when the text does not match the pattern", function() {
+    it("should return false when the text does not match the pattern", () => {
       const pattern = "a";
       const text = "c";
       expect(matchOne(pattern, text)).to.equal(false);
     });
-    it("should match any character against a '.'", function() {
+    it("should match any character against a '.'", () => {
       expect(matchOne(".", "c")).to.equal(true);
       expect(matchOne(".", "q")).to.equal(true);
     });
   });
 
-  describe("search", function() {
-    it("should return true if given an empty pattern", function() {
-      expect(search("", "abc")).to.equal(true);
-      expect(search("", "cab")).to.equal(true);
+  describe("match", () => {
+    it("should return true if given an empty pattern", () => {
+      expect(match("", "abc")).to.equal(true);
+      expect(match("", "cab")).to.equal(true);
     });
-    it("should match an empty string to the end of line pattern '$'", function() {
-      expect(search("", "$")).to.equal(true);
+    it("should match an empty string to the end of line pattern '$'", () => {
+      expect(match("", "$")).to.equal(true);
     });
-
-    describe("patterns starting with '^'", function() {
-      it("should match a longer sequence of characters", function() {
+  });
+  describe("search", () => {
+    describe("patterns starting with '^'", () => {
+      it("should match a longer sequence of characters", () => {
         expect(search("^please work", "please work")).to.equal(true);
         expect(search("^good test", "good test")).to.equal(true);
 
         expect(search("^bad", "test")).to.equal(false);
         expect(search("^also bad test", "also bad tst")).to.equal(false);
       });
-      it("should still support the wildcard character '.'", function() {
+      it("should still support the wildcard character '.'", () => {
         expect(search("^a good t.st", "a good test")).to.equal(true);
         expect(search("^an.ther g..d test", "another good test")).to.equal(true);
 
         expect(search("^b.d test", "baad test")).to.equal(false);
       });
-      it("should still support end of string character '$'", function() {
+      it("should still support end of string character '$'", () => {
         expect(search("^match end$", "match end")).to.equal(true);
 
         expect(search("^match$", "match end")).to.equal(false);
       });
-      it("should support partial matches", function() {
+      it("should support partial matches", () => {
         expect(search("^partial", "partial match")).to.equal(true);
         expect(search("^good", "good test")).to.equal(true);
 
-        expect(search("^od te", "good test")).to.equal(false);
-        expect(search("^oos", "good test")).to.equal(false);
+        expect(search("^bad", "ba test")).to.equal(false);
       });
     });
-    describe("patterns not starting with '^'", function() {
-      it("should match a sequence of characters inside a larger text body", function() {
+    describe("patterns not starting with '^'", () => {
+      it("should match a sequence of characters inside a larger text body", () => {
         expect(search("match", "this is a match")).to.equal(true);
         expect(search("what", "this is what we are doing")).to.equal(true);
         expect(search("is what", "this is what we are doing")).to.equal(true);
 
         expect(search("iswhat", "this is what we are doing")).to.equal(false);
         expect(search("blah", "this is what we are doing")).to.equal(false);
+      });
+    });
+  });
+  describe("should match 0 or 1 of the following character a '?'", () => {
+    describe("matchQuestion", () => {
+      it("supports a pattern starting with '?", () => {
+        expect(matchQuestion("i?", "this is i")).to.equal(true);
+        expect(matchQuestion("i?", "this is p")).to.equal(true);
+        expect(matchQuestion("j?", "works with any character")).to.equal(true);
+
+        expect(matchQuestion("w?orks", "works")).to.equal(true);
+        expect(matchQuestion("w?orks", "work")).to.equal(false);
+
+        expect(matchQuestion("w?orks", "work")).to.equal(false);
+      });
+    });
+    describe("search", () => {
+      it("supports `?` anywhere in the pattern", () => {
+        expect(search("this is? it", "this is it")).to.equal(true);
+        expect(search("this is? it", "this is it")).to.equal(true);
+        expect(search("this is? it", "this i it")).to.equal(true);
+
+        expect(search("is? it? r?e?a?lly", "is it really")).to.equal(true);
+        expect(search("is? it? r?e?a?lly", "i i lly")).to.equal(true);
+
+        expect(search("is? it? r?e?a?lly", "i i ly")).to.equal(false);
       });
     });
   });
