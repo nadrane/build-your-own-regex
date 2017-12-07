ee929cc9f97e764545e9e84f0766a31068ac2b94
Nicholas Drane <nicholasdrane@gmail.com>
2017-12-07 09:19:50 -0600

    style(solution): autoformat

--- solution.js
+++ solution.js
@@ -10,7 +10,7 @@ function search(pattern, text) {
   if (pattern[0] === "^") {
     return match(pattern.slice(1), text);
   } else {
-    return match('.*' + pattern, text)
+    return match(".*" + pattern, text);
   }
 }
 
@@ -21,8 +21,8 @@ function match(pattern, text) {
     return matchQuestion(pattern, text);
   } else if (pattern[1] === "*") {
     return matchStar(pattern, text);
-  } else if (pattern[0] === '(') {
-    return matchGroup(pattern, text)
+  } else if (pattern[0] === "(") {
+    return matchGroup(pattern, text);
   } else {
     return matchOne(pattern[0], text[0]) && match(pattern.slice(1), text.slice(1));
   }
@@ -43,17 +43,28 @@ function matchStar(pattern, text) {
 }
 
 function matchGroup(pattern, text) {
-  const groupEnd = pattern.indexOf(')')
-  const groupPattern = pattern.slice(1, groupEnd)
-  if (pattern[groupEnd + 1] === '?') {
-    const remainderPattern = pattern.slice(groupEnd + 2) // +2 needed to slice off the ')?'
-    return match(groupPattern, text.slice(0, groupPattern.length)) && match(remainderPattern, text.slice(groupPattern.length)) || match(remainderPattern, text)
-  } else if (pattern[groupEnd + 1] === '*') {
-    const remainderPattern = pattern.slice(groupEnd + 2) // +2 needed to slice off the ')*'
-    return match(groupPattern, text.slice(0, groupPattern.length)) && match(pattern, text.slice(groupPattern.length)) || match(remainderPattern, text)
+  const groupEnd = pattern.indexOf(")");
+  const groupPattern = pattern.slice(1, groupEnd);
+  if (pattern[groupEnd + 1] === "?") {
+    const remainderPattern = pattern.slice(groupEnd + 2); // +2 needed to slice off the ')?'
+    return (
+      (match(groupPattern, text.slice(0, groupPattern.length)) &&
+        match(remainderPattern, text.slice(groupPattern.length))) ||
+      match(remainderPattern, text)
+    );
+  } else if (pattern[groupEnd + 1] === "*") {
+    const remainderPattern = pattern.slice(groupEnd + 2); // +2 needed to slice off the ')*'
+    return (
+      (match(groupPattern, text.slice(0, groupPattern.length)) &&
+        match(pattern, text.slice(groupPattern.length))) ||
+      match(remainderPattern, text)
+    );
   } else {
-    const remainderPattern = pattern.slice(groupEnd + 1)  // +1 needed to slice off the ')'
-    return match(groupPattern, text.slice(0, groupPattern.length)) && match(remainderPattern, text.slice(groupPattern.length))
+    const remainderPattern = pattern.slice(groupEnd + 1); // +1 needed to slice off the ')'
+    return (
+      match(groupPattern, text.slice(0, groupPattern.length)) &&
+      match(remainderPattern, text.slice(groupPattern.length))
+    );
   }
 }
 
